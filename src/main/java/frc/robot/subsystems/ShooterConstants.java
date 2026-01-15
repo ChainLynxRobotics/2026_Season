55ebf72c9ca37b923f4619f76b07b22cdb9ff328
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-01-14 22:46:25 -0800

    tune some constants

--- src/main/java/frc/robot/subsystems/ShooterConstants.java
+++ src/main/java/frc/robot/subsystems/ShooterConstants.java
@@ -10,12 +10,12 @@ import edu.wpi.first.units.measure.*;
 public class ShooterConstants {
 
   public static final Distance kFlywheelRadius = Inches.of(2);
-  public static final MomentOfInertia kFlywheelMOI = KilogramSquareMeters.of(0.0003511676);
+  public static final MomentOfInertia kFlywheelMOI = KilogramSquareMeters.of(0.0009885367);
   public static final int kFlywheelCANId = 25;
   private static final double kFlywheelS = 0;
-  private static final double kFlywheelA = 0;
-  private static final double kFlywheelV = 0;
-  private static final double kFlywheelP = 0;
+  private static final double kFlywheelA = 0.0095487;
+  private static final double kFlywheelV = 0.12371;
+  private static final double kFlywheelP = 0.095152;
   private static final double kFlywheelI = 0;
   private static final double kFlywheelD = 0;
   public static final double kFlywheelGearRatio = 1;
@@ -31,6 +31,9 @@ public class ShooterConstants {
   private static TalonFXConfiguration generateFlywheelConfig() {
     var config = new TalonFXConfiguration().withSlot0(kFlywheelSlot0Configs);
     config.MotorOutput.NeutralMode = NeutralModeValue.Coast;
+    config.MotionMagic.MotionMagicAcceleration = 500;
+    config.MotionMagic.MotionMagicCruiseVelocity = 250;
+    config.MotionMagic.MotionMagicJerk = 75;
     return config;
   }
 
