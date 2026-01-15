b5d73a7583a4d2b63fa3b69385da2c9bfa4a7088
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-01-14 22:58:47 -0800

    move shooter to its own folder

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -19,7 +19,7 @@ import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.CommandSwerveDrivetrain;
-import frc.robot.subsystems.Shooter;
+import frc.robot.subsystems.Shooter.Shooter;
 import org.ironmaple.simulation.SimulatedArena;
 
 @Logged
