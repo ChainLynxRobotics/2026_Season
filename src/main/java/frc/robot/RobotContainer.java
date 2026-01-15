46ac7d68d4e56616a11e032c70819a82708845cb
Ben Brown <benvubrown@gmail.com>
2026-01-14 19:10:45 -0800

    Updated swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -26,12 +26,12 @@ import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
-import frc.robot.subsystems.CommandSwerveDrivetrain;
 import frc.robot.subsystems.Shooter.Shooter;
 import frc.robot.subsystems.Shooter.ShooterConstants;
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.motorsims.SimulatedBattery;
 import frc.robot.subsystems.Drivetrain.DrivetrainConstants;
+import frc.robot.subsystems.Swerve.CommandSwerveDrivetrain;
 
 @Logged
 public class RobotContainer {
