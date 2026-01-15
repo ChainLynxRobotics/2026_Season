b0d0defc935ea626ac583da1c14d23b275b6c3c7
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-01-14 16:48:18 -0800

    Add kDT in constants

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,9 @@
+package frc.robot;
+
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.units.measure.*;
+
+public class Constants {
+  public static final Time kDT = Seconds.of(0.02);
+}
