d537e6d2db40884593903705c6603047730ea299
Ben Brown <benvubrown@gmail.com>
2026-01-17 14:27:07 -0800

    Added second indexer motor

--- src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
@@ -1,29 +1,39 @@
 package frc.robot.subsystems.Indexer;
 
 import com.ctre.phoenix6.hardware.TalonFX;
-
 import edu.wpi.first.epilogue.Logged;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class IndexerSubsystem extends SubsystemBase{
-    private TalonFX indexerMotor = new TalonFX(0);
+public class IndexerSubsystem extends SubsystemBase {
+  private TalonFX indexerChuteMotor = new TalonFX(0);
+  private TalonFX indexerFeedMotor = new TalonFX(0);
 
-    public IndexerSubsystem(){
+  public IndexerSubsystem() {}
 
-    } 
-   
-    @Logged
-    public double getIndexerPosition(){
-        return indexerMotor.getPosition().getValueAsDouble();
-    }
+  @Logged
+  public double getIndexerChutePosition() {
+    return indexerChuteMotor.getPosition().getValueAsDouble();
+  }
 
-    @Logged
-    public double getIndexerVelocity(){
-        return indexerMotor.getVelocity().getValueAsDouble();
-    }
+  @Logged
+  public double getIndexerChuteVelocity() {
+    return indexerChuteMotor.getVelocity().getValueAsDouble();
+  }
 
-    public Command spin(){
-        return run(() -> indexerMotor.setVoltage(0));
-    }
+  @Logged
+  public double getIndexerFeedPosition() {
+    return indexerFeedMotor.getPosition().getValueAsDouble();
+  }
+
+  @Logged
+  public double getIndexerFeedVelocity() {
+    return indexerFeedMotor.getVelocity().getValueAsDouble();
+  }
+
+  public Command spin() {
+    return Commands.parallel(
+        run(() -> indexerChuteMotor.setVoltage(0)), run(() -> indexerFeedMotor.setVoltage(0)));
+  }
 }
