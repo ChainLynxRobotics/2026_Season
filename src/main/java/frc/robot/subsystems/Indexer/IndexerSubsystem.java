e9246416024a44021fa2c50601b8b30ec2c8e2b4
Ben Brown <benvubrown@gmail.com>
2026-01-17 14:27:07 -0800

    Added second indexer motor

--- src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
@@ -14,6 +14,7 @@ import edu.wpi.first.math.system.plant.LinearSystemId;
 import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
