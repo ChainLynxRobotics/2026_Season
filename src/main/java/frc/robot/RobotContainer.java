2597a7965fbf818370fdddebee7b370e724fadbe
Red laptop <devices@chainlynx8248.com>
2026-01-17 12:37:41 -0800

    add projectile sims

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
+import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
 
 import com.ctre.phoenix6.swerve.SwerveModule.DriveRequestType;
 import com.ctre.phoenix6.swerve.SwerveRequest;
@@ -42,12 +43,17 @@ public class RobotContainer {
 
   private final Telemetry logger = new Telemetry(MaxSpeed);
 
-  private final CommandXboxController joystick = new CommandXboxController(0);
+  private final CommandXboxController driveJoystick = new CommandXboxController(0);
+  private final CommandXboxController opperatorJoystick = new CommandXboxController(1);
 
   public final CommandSwerveDrivetrain drivetrain = TunerConstants.createDrivetrain();
 
   @Logged(name = "Shooter")
-  public final Shooter shooter = new Shooter(() -> drivetrain.getState().Pose);
+  public final Shooter shooter =
+      new Shooter(
+          () -> drivetrain.getState().Pose,
+          drivetrain::getSimPose,
+          () -> drivetrain.getState().Speeds);
 
   public RobotContainer() {
     configureBindings();
@@ -69,11 +75,12 @@ public class RobotContainer {
             () ->
                 drive
                     .withVelocityX(
-                        -joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
+                        -driveJoystick.getLeftY()
+                            * MaxSpeed) // Drive forward with negative Y (forward)
                     .withVelocityY(
-                        -joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                        -driveJoystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
                     .withRotationalRate(
-                        -joystick.getRightX()
+                        -driveJoystick.getRightX()
                             * MaxAngularRate) // Drive counterclockwise with negative X (left)
             ));
 
@@ -83,30 +90,43 @@ public class RobotContainer {
     RobotModeTriggers.disabled()
         .whileTrue(drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
-    joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-    joystick
+    driveJoystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+    driveJoystick
         .b()
         .whileTrue(
             drivetrain.applyRequest(
                 () ->
                     point.withModuleDirection(
-                        new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
+                        new Rotation2d(-driveJoystick.getLeftY(), -driveJoystick.getLeftX()))));
 
     // Run SysId routines when holding back/start and X/Y.
     // Note that each routine should be run exactly once in a single log.
-    joystick.back().and(joystick.y()).whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
-    joystick.back().and(joystick.x()).whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
-    joystick.start().and(joystick.y()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
-    joystick.start().and(joystick.x()).whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
+    driveJoystick
+        .back()
+        .and(driveJoystick.y())
+        .whileTrue(drivetrain.sysIdDynamic(Direction.kForward));
+    driveJoystick
+        .back()
+        .and(driveJoystick.x())
+        .whileTrue(drivetrain.sysIdDynamic(Direction.kReverse));
+    driveJoystick
+        .start()
+        .and(driveJoystick.y())
+        .whileTrue(drivetrain.sysIdQuasistatic(Direction.kForward));
+    driveJoystick
+        .start()
+        .and(driveJoystick.x())
+        .whileTrue(drivetrain.sysIdQuasistatic(Direction.kReverse));
 
     // Reset the field-centric heading on left bumper press.
-    joystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
+    driveJoystick.leftBumper().onTrue(drivetrain.runOnce(drivetrain::seedFieldCentric));
 
     drivetrain.registerTelemetry(logger::telemeterize);
 
-    joystick.a().whileTrue(shooter.flywheelSysid());
-    joystick.b().onTrue(shooter.setFlywheelVelocity(RotationsPerSecond.of(20)));
-    joystick.x().onTrue(shooter.setHoodAngle(Degrees.of(60)));
+    opperatorJoystick.a().whileTrue(shooter.flywheelSysid());
+    opperatorJoystick.b().onTrue(shooter.setFlywheelVelocity(RotationsPerSecond.of(20)));
+    opperatorJoystick.x().onTrue(shooter.setHoodAngle(Degrees.of(60)));
+    opperatorJoystick.y().onTrue(runOnce(shooter::shootSimulatedProjectile));
   }
 
   public Command getAutonomousCommand() {
