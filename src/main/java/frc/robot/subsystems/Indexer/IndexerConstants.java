ee2b2bf1b39e7d6a5364ea960a263b2a7711c614
Red laptop <devices@chainlynx8248.com>
2026-01-20 16:37:02 -0800

    Added empty tuning constants

--- src/main/java/frc/robot/subsystems/Indexer/IndexerConstants.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerConstants.java
@@ -1,3 +1,26 @@
 package frc.robot.subsystems.Indexer;
 
-public class IndexerConstants {}
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+
+public class IndexerConstants {
+  private static double kP;
+  private static double kI;
+  private static double kD;
+  private static double kV;
+  private static double kA;
+  private static double kG;
+  private static double kS;
+  private static double kMaxVelocity;
+  public static Slot0Configs kIndexerSlot0Config =
+      new Slot0Configs()
+          .withKP(kP)
+          .withKI(kI)
+          .withKD(kD)
+          .withKV(kV)
+          .withKA(kA)
+          .withKG(kG)
+          .withKS(kS);
+  public static MotionMagicConfigs kIndexerMotionMagicConfig =
+      new MotionMagicConfigs().withMotionMagicCruiseVelocity(kMaxVelocity);
+}
