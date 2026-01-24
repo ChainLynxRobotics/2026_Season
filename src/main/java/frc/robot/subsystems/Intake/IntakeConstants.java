4f79dac54bf225a6a224713e1a64b7e2a96c9bee
Ben Brown <benvubrown@gmail.com>
2026-01-23 18:44:53 -0800

    Added proper PID and formatting

--- src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -1,26 +1,32 @@
 package frc.robot.subsystems.Intake;
 
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+
 import com.ctre.phoenix6.configs.Slot0Configs;
-
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 
-public class IntakeConstants{
-    public static Angle kIntakeLowAngle;
-    public static Angle kIntakeHighAngle;
-    
-    public static Slot0Configs intakeHeightSlot0Config = new Slot0Configs();
+public class IntakeConstants {
+  public static Angle kIntakeLowAngle;
+  public static Angle kIntakeHighAngle;
 
-    public enum IntakeState{
-        HIGH(kIntakeHighAngle),LOW(kIntakeLowAngle);
+  public static AngularVelocity kGoalIntakeSpinVelocity = RotationsPerSecond.of(0.0);
 
-        public final Angle angle;
+  public static Slot0Configs intakeHeightSlot0Config =
+      new Slot0Configs().withKP(0).withKI(0).withKD(0).withKV(0).withKA(0).withKG(0).withKS(0);
 
-        IntakeState(Angle angle){
-            this.angle=angle;
-        }
+  public enum IntakeState {
+    HIGH(kIntakeHighAngle),
+    LOW(kIntakeLowAngle);
 
-        public Angle getAngle(){
-            return angle;
-        }
+    public final Angle angle;
+
+    IntakeState(Angle angle) {
+      this.angle = angle;
     }
+
+    public Angle getAngle() {
+      return angle;
+    }
+  }
 }
