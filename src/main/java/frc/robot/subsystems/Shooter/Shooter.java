22d174f4505e6f885e381eafa58905281c7c2f03
Red laptop <devices@chainlynx8248.com>
2026-01-23 16:26:14 -0800

    remove shooter lut testing

--- src/main/java/frc/robot/subsystems/Shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter/Shooter.java
@@ -358,7 +358,7 @@ public class Shooter extends SubsystemBase implements AutoCloseable {
         && isWithinTolerance(getHoodPosition(), Degrees.of(90), Degrees.of(0.01))) {
       hoodMotor.setPosition(Degrees.of(90));
     }
-    if (timeLastBall == 25) {
+    if (timeLastBall == -1) {
       this.shootSimulatedProjectile();
       timeLastBall = 0;
     } else {
