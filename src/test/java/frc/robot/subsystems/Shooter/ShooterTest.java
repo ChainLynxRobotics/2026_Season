255e6241b477b91f82d37a06d7e9f6c0237041ad
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-01-27 15:56:18 -0800

    fix tests

--- src/test/java/frc/robot/subsystems/Shooter/ShooterTest.java
+++ src/test/java/frc/robot/subsystems/Shooter/ShooterTest.java
@@ -35,33 +35,7 @@ public class ShooterTest {
   @Test
   void testGetFlywheelPosition() {
     shooter.flywheelSim.setAngle(1);
+    shooter.simulationPeriodic();
     assertEquals(shooter.getFlywheelPosition().in(Radians), 1 * kHoodGearRatio, 1e-6);
   }
-  */
-
-  
-  @Test
-  void TestFlywheelRotation() {
-    shooter.flywheelSim.setInputVoltage(RobotController.getBatteryVoltage());
-
-    shooter.flywheelSim.setAngularVelocity(RadiansPerSecond.of(20));
-
-    for (int i = 0; i < 100; ++i) {
-      Timer.delay(0.020);
-      DCMotorSim.setInputVoltage(simulatedMotor.getMotorVoltage());
-      System.out.println(simulatedMotor.getMotorVoltage());
-      DCMotorSim.update(0.020);
-
-      simulatedMotor.setRawRotorPosition(DCMotorSim.getAngularPosition().times(kGearRatio));
-      simulatedMotor.setRotorVelocity(DCMotorSim.getAngularVelocity().times(kGearRatio));
-      // System.out.println(climber.atSetpoint() ? "yes" : "no");
-      System.out.println(shooter.flywheelSim.getAngularVelocity());
-      if (climber.atSetpoint()) {
-        System.out.println(i + " ticks");
-        break;
-      }
-    }
-  }
-  assertTrue(climber.atSetpoint());
 }
-
