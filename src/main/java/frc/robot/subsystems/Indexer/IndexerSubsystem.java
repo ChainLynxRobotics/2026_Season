f539d1bca7022ac81394ec4a89a8b4077cb69a14
Red laptop <devices@chainlynx8248.com>
2026-01-27 17:31:44 -0800

    Added SysId and updated constants

--- src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
@@ -1,37 +1,78 @@
 package frc.robot.subsystems.Indexer;
+
+import static edu.wpi.first.units.Units.*;
 import static frc.robot.subsystems.Indexer.IndexerConstants.*;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
 import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.units.*;
+import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularVelocity;
 import edu.wpi.first.units.measure.Voltage;
-import edu.wpi.first.wpilibj2.command.*;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 
 public class IndexerSubsystem extends SubsystemBase {
-  private TalonFX indexerMotor = new TalonFX(0);
+  private TalonFX indexerMotor;
   private TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
+  private VelocityVoltage indexerControl = new VelocityVoltage(kGoalIndexerVelocity);
+  private VoltageOut voltageOut = new VoltageOut(0.0);
 
-  private SysIdRoutine indexerSysIdRoutine = new SysIdRoutine(new SysIdRoutine.Config(Voltage.of(0).per(Seconds), Volts.of(2), Seconds.of(6)), null);
+  private SysIdRoutine indexerSysIdRoutine =
+      new SysIdRoutine(
+          new SysIdRoutine.Config(Volts.of(0).per(Seconds), Volts.of(2), Seconds.of(6)),
+          new SysIdRoutine.Mechanism(
+              volts -> indexerMotor.setControl(voltageOut.withOutput(volts.in(Volts))),
+              log ->
+                  log.motor("Indexer Motor")
+                      .angularPosition(this.getAngularPosition())
+                      .angularVelocity(this.getAngularVelocity())
+                      .voltage(this.getVoltage()),
+              null));
 
-  public IndexerSubsystem() {
-    talonFXConfiguration.Slot0=kIndexerSlot0Config;
-    talonFXConfiguration.MotionMagic=kIndexerMotionMagicConfig;
+  public IndexerSubsystem(TalonFX indexerMotor) {
+    this.indexerMotor = indexerMotor;
+    talonFXConfiguration.Slot0 = kIndexerSlot0Config;
+    talonFXConfiguration.MotionMagic = kIndexerMotionMagicConfig;
     indexerMotor.getConfigurator().apply(talonFXConfiguration);
   }
 
   @Logged
-  public double getIndexerPosition() {
+  public double getPosition() {
     return indexerMotor.getPosition().getValueAsDouble();
   }
 
+  public Angle getAngularPosition() {
+    return indexerMotor.getPosition().getValue();
+  }
+
   @Logged
-  public double getIndexerVelocity() {
+  public AngularVelocity getAngularVelocity() {
+    return indexerMotor.getVelocity().getValue();
+  }
+
+  @Logged
+  public double getVelocity() {
     return indexerMotor.getVelocity().getValueAsDouble();
   }
 
+  @Logged
+  public Voltage getVoltage() {
+    return indexerMotor.getMotorVoltage().getValue();
+  }
+
+  public void runSysId() {
+    run(() -> indexerSysIdRoutine.dynamic(Direction.kForward))
+        .andThen(indexerSysIdRoutine.dynamic(Direction.kReverse))
+        .andThen(indexerSysIdRoutine.quasistatic(Direction.kForward))
+        .andThen(indexerSysIdRoutine.quasistatic(Direction.kReverse));
+  }
+
   public Command spin() {
-    return run(()->indexerMotor.set(0));
+    return run(() -> indexerMotor.setControl(indexerControl));
   }
 }
