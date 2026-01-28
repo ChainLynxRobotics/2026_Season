3f624d5235eeb795a2af81ab56d44e5bf7d903b2
Kenney4237 <kenneth.yang4237@gmail.com>
2026-01-27 18:07:49 -0800

    Unfinished. For time to continue working on

--- src/test/subsystems/ShooterTest.java
+++ src/test/subsystems/ShooterTest.java
@@ -2,11 +2,13 @@ import static edu.wpi.first.units.Units.Radians;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.RobotController;
 import frc.robot.subsystems.Shooter.Shooter;
 import org.junit.jupiter.api.BeforeEach;
-
+import edu.wpi.first.wpilibj.Timer;
 public class ShooterTest {
   Shooter shooter;
+
 
   @BeforeEach
   void setup() {
@@ -19,10 +21,37 @@ public class ShooterTest {
   void shutdown() throws Exception {
     shooter.close(); // destroy our intake object
   }
-
+  /*
   @Test
   void testGetFlywheelPosition() {
     shooter.flywheelSim.setAngle(1);
     assertEquals(shooter.getFlywheelPosition().in(Radians), 1 * kHoodGearRatio, 1e-6);
   }
+  */
+
+  
+  @Test
+  void TestFlywheelRotation() {
+    shooter.flywheelSim.setInputVoltage(RobotController.getBatteryVoltage());
+
+    shooter.flywheelSim.setAngularVelocity(RadiansPerSecond.of(20));
+
+    for (int i = 0; i < 100; ++i) {
+      Timer.delay(0.020);
+      DCMotorSim.setInputVoltage(simulatedMotor.getMotorVoltage());
+      System.out.println(simulatedMotor.getMotorVoltage());
+      DCMotorSim.update(0.020);
+
+      simulatedMotor.setRawRotorPosition(DCMotorSim.getAngularPosition().times(kGearRatio));
+      simulatedMotor.setRotorVelocity(DCMotorSim.getAngularVelocity().times(kGearRatio));
+      // System.out.println(climber.atSetpoint() ? "yes" : "no");
+      System.out.println(shooter.flywheelSim.getAngularVelocity());
+      if (climber.atSetpoint()) {
+        System.out.println(i + " ticks");
+        break;
+      }
+    }
+  }
+  assertTrue(climber.atSetpoint());
 }
+
