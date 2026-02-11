7aff0669522b1bb497e3e472135f1edd2c2d0a6c
Wrenn54 <benvubrown@gmail.com>
2026-02-10 18:50:07 -0800

    Fixed formatting

--- src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeConstants.java
@@ -1,46 +1,50 @@
 package frc.robot.subsystems.Intake;
 
 import static edu.wpi.first.units.Units.*;
-import static edu.wpi.first.units.Units.RotationsPerSecond;
 
 import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.units.measure.AngularAcceleration;
 import edu.wpi.first.units.measure.AngularVelocity;
 
 public class IntakeConstants {
-  public static Angle kIntakeLowAngle = Degrees.of(124);
-  public static Angle kIntakeHighAngle = Rotations.of(0);
+  public static Angle kIntakeLowAngle = Degree.of(0);
+  public static Angle kIntakeHighAngle = Degree.of(124);
   public static AngularVelocity kGoalIntakeSpinVelocity = RotationsPerSecond.of(10);
-  public static double kOutputToInputSpinGearRatio = 1.0 / 3;
-  public static double kOutputToInputHeightGearRatio = 1.0 / 60;
+  public static double kInputToOutputSpinGearRatio = 3;
+  public static double kInputToOutputHeightGearRatio = 60;
   public static double kT = 0.02;
+  public static double kIntakeLengthFromPivot = 0.3347841158;
 
-  public static double kIntakeHeightMaxVelocity = 100;
-  public static double kIntakeHeightMaxAcceleration = 100;
+  public static AngularVelocity kIntakeHeightMaxVelocity = RotationsPerSecond.of(100);
+  public static AngularAcceleration kIntakeHeightMaxAcceleration =
+      RotationsPerSecondPerSecond.of(100);
 
   public static MotionMagicConfigs kIntakeHeightMotionMagic =
       new MotionMagicConfigs()
           .withMotionMagicCruiseVelocity(kIntakeHeightMaxVelocity)
           .withMotionMagicAcceleration(kIntakeHeightMaxAcceleration);
 
-  private static double kHeightP = 5;
+  private static double kHeightP = 0;
   private static double kHeightI = 0;
   private static double kHeightD = 0;
-  //   private static double kHeightV = 0;
-  //   private static double kHeightA = 0;
-  //   private static double kHeightG = 0;
-  //   private static double kHeightS = 0;
+  public static double kHeightV = 7.47;
+  public static double kHeightA = 0.02;
+  private static double kHeightG = 0.09;
+  private static double kHeightS = 0;
 
   public static Slot0Configs kIntakeHeightSlot0Config =
-      new Slot0Configs().withKP(kHeightP).withKI(kHeightI).withKD(kHeightD)
-      //   .withKV(kHeightV)
-      //   .withKA(kHeightA)
-      //   .withKG(kHeightG)
-      //   .withKS(kHeightS)
-      ;
+      new Slot0Configs()
+          .withKP(kHeightP)
+          .withKI(kHeightI)
+          .withKD(kHeightD)
+          .withKV(kHeightV)
+          .withKA(kHeightA)
+          .withKG(kHeightG)
+          .withKS(kHeightS);
 
-  private static double kSpinP = 0.65;
+  private static double kSpinP = 2;
   private static double kSpinI = 0.25;
   private static double kSpinD = 0;
   private static double kSpinV = 0;
@@ -57,8 +61,8 @@ public class IntakeConstants {
       ;
 
   public enum IntakeHeightState {
-    HIGH(kIntakeHighAngle),
-    LOW(kIntakeLowAngle);
+    HIGH(kIntakeHighAngle.times(kInputToOutputHeightGearRatio)),
+    LOW(kIntakeLowAngle.times(kInputToOutputHeightGearRatio));
 
     public final Angle angle;
 
