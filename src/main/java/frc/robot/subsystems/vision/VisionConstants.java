880c7210dda3715f385c92575ba2f75d4f5a2216
Red laptop <devices@chainlynx8248.com>
2026-02-10 19:18:59 -0800

    Made Vision pick the right hub

--- src/main/java/frc/robot/subsystems/vision/VisionConstants.java
+++ src/main/java/frc/robot/subsystems/vision/VisionConstants.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.numbers.*;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.wpilibj.DriverStation;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -31,7 +32,13 @@ public class VisionConstants {
               new Transform3d(0.268, 0.19684, 0.5715, new Rotation3d(0, 0, 0))
               /*new Transform3d(0, -0.4347972, 0.54864, new Rotation3d(0, 0, Math.PI / 2))*/ ));
 
-  public static final Pose3d kHubLocation =
-      new Pose3d(Inches.of(469.11), Inches.of(158.84), Inches.of(72), new Rotation3d()); // );
+  public static Pose3d getHubLocation() {
+    if (DriverStation.getAlliance().get().equals(DriverStation.Alliance.Red)) {
+      return new Pose3d(Inches.of(469.11), Inches.of(158.84), Inches.of(72), new Rotation3d());
+    }
+    return new Pose3d(Inches.of(182.11), Inches.of(158.84), Inches.of(72), new Rotation3d());
+  }
+
+  public static final Pose3d kHubLocation = getHubLocation();
   public static final Matrix<N3, N1> kBaseDeviation = VecBuilder.fill(1.5, 1.5, 10);
 }
