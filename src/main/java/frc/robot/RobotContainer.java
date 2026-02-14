8f8eab48262df46f902486a555364a8a9d6dd316
Kenney4237 <kenneth.yang4237@gmail.com>
2026-02-13 16:15:48 -0800

    Finished Path planner paths.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,6 +20,7 @@ import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -67,6 +68,11 @@ public class RobotContainer {
 
   public RobotContainer() {
     NamedCommands.registerCommand("goToHub", goToHub());
+    NamedCommands.registerCommand("Climb", new PrintCommand("Implement it plz"));
+    NamedCommands.registerCommand("Shoot Balls", new PrintCommand("Implement it plz"));
+    NamedCommands.registerCommand("Stop Shoot", new PrintCommand("Implement it plz"));
+    NamedCommands.registerCommand("Scoop", new PrintCommand("Implement it plz"));
+    NamedCommands.registerCommand("Stop Scoop", new PrintCommand("Implement it plz"));
 
     configureBindings();
     // shooter.setDefaultCommand(shooter.runShooterControl());
@@ -177,7 +183,7 @@ public class RobotContainer {
   }
 
   public Command getAutonomousCommand() {
-    return new PathPlannerAuto("GoUP");
+    return new PathPlannerAuto("0");
 
     // Simple drive forward auton
     /*
