5ed1c11a0000fdefe62d8ea4fb3690dcbbb9465c
Wrenn54 <benvubrown@gmail.com>
2026-02-16 15:10:17 -0800

    Set up sim an d sysId

--- src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
+++ src/main/java/frc/robot/subsystems/Indexer/IndexerSubsystem.java
@@ -5,69 +5,97 @@ import static frc.robot.subsystems.Indexer.IndexerConstants.*;
 
 import com.ctre.phoenix6.configs.TalonFXConfiguration;
 import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.controls.VoltageOut;
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.sim.TalonFXSimState;
 import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.math.system.plant.LinearSystemId;
+import edu.wpi.first.units.measure.Voltage;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
+import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
+
+import org.ironmaple.simulation.motorsims.SimulatedBattery;
 
 public class IndexerSubsystem extends SubsystemBase {
   private TalonFX indexerMotor;
-  private TalonFXConfiguration talonFXConfiguration = new TalonFXConfiguration();
+  private TalonFXConfiguration indexerConfiguration = new TalonFXConfiguration();
   private VelocityVoltage indexerControl = new VelocityVoltage(kGoalIndexerVelocity);
 
-  // private SysIdRoutine indexerSysIdRoutine =
-  //     new SysIdRoutine(
-  //         new SysIdRoutine.Config(Volts.of(0).per(Seconds), Volts.of(2), Seconds.of(6)),
-  //         new SysIdRoutine.Mechanism(
-  //             volts -> indexerMotor.setControl(voltageOut.withOutput(volts.in(Volts))),
-  //             log ->
-  //                 log.motor("Indexer Motor")
-  //                     .angularPosition(this.getAngularPosition())
-  //                     .angularVelocity(this.getAngularVelocity())
-  //                     .voltage(this.getVoltage()),
-  //             null));
+  private DCMotor x44Gearbox = DCMotor.getKrakenX44Foc(1);
+  private TalonFXSimState indexerSimState;
+  private DCMotorSim indexerSim =
+      new DCMotorSim(LinearSystemId.createDCMotorSystem(x44Gearbox, 0.001, 1), x44Gearbox);
+
+      private VoltageOut voltageOut = new VoltageOut(0.0);
+
+      private SysIdRoutine indexerSysIdRoutine =
+      new SysIdRoutine(
+          new SysIdRoutine.Config(Volts.of(0.5).per(Seconds), Volts.of(2), Seconds.of(6)),
+          new SysIdRoutine.Mechanism(
+              volts -> indexerMotor.setControl(voltageOut.withOutput(volts.in(Volts))),
+              log ->
+                  log.motor("Leader")
+                      .angularPosition(Rotations.of(this.getIndexerPosition()))
+                      .angularVelocity(RotationsPerSecond.of(this.getIndexerVelocity()))
+                      .voltage(this.getIndexerVoltage()),
+              this));
 
   public IndexerSubsystem(TalonFX indexerMotor) {
     this.indexerMotor = indexerMotor;
-    talonFXConfiguration.Slot0 = kIndexerSlot0Config;
-    talonFXConfiguration.MotionMagic = kIndexerMotionMagicConfig;
-    indexerMotor.getConfigurator().apply(talonFXConfiguration);
+
+    indexerConfiguration.Slot0 = kIntakeIndexerSlot0Config;
+    indexerMotor.getConfigurator().apply(indexerConfiguration);
+
+    indexerSimState = indexerMotor.getSimState();
+  }
+
+   public void runSysId() {
+    run(() -> indexerSysIdRoutine.dynamic(Direction.kForward))
+        .andThen(indexerSysIdRoutine.dynamic(Direction.kReverse))
+        .andThen(indexerSysIdRoutine.quasistatic(Direction.kForward))
+        .andThen(indexerSysIdRoutine.quasistatic(Direction.kReverse));
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    indexerSimState.setSupplyVoltage(SimulatedBattery.getBatteryVoltage());
+    indexerSim.setInputVoltage(indexerSimState.getMotorVoltage());
+    indexerSim.update(kT);
+    indexerSimState.setRawRotorPosition(indexerSim.getAngularPositionRotations());
+    indexerSimState.setRotorVelocity(indexerSim.getAngularVelocityRPM() / 60.0);
   }
 
   @Logged
-  public double getPosition() {
-    return indexerMotor.getPosition().getValueAsDouble();
-  }
-
-  public Angle getAngularPosition() {
-    return indexerMotor.getPosition().getValue();
+  private Voltage getIndexerVoltage() {
+    return indexerMotor.getMotorVoltage().getValue();
   }
 
   @Logged
-  public double getVelocity() {
+  public double getIndexerVelocity() {
     return indexerMotor.getVelocity().getValueAsDouble();
   }
 
   @Logged
-  public AngularVelocity getAngularVelocity() {
-    return indexerMotor.getVelocity().getValue();
+  public double getIndexerPosition() {
+    return indexerMotor.getPosition().getValueAsDouble();
   }
 
-  // @Logged
-  // public Voltage getVoltage() {
-  //   return indexerMotor.getMotorVoltage().getValue();
-  // }
+  @Logged
+  public double getSimIndexerPosition() {
+    return indexerSim.getAngularPositionRotations();
+  }
 
-  // public void runSysId() {
-  //   run(() -> indexerSysIdRoutine.dynamic(Direction.kForward))
-  //       .andThen(indexerSysIdRoutine.dynamic(Direction.kReverse))
-  //       .andThen(indexerSysIdRoutine.quasistatic(Direction.kForward))
-  //       .andThen(indexerSysIdRoutine.quasistatic(Direction.kReverse));
-  // }
+  @Logged
+  public double getSimIndexerVelocity() {
+    return indexerSim.getAngularVelocityRPM() / 60.0;
+  }
 
+  @Logged
   public Command spin() {
-    return run(() -> indexerMotor.setControl(indexerControl));
+    return runOnce(() -> indexerMotor.setControl(indexerControl));
   }
 }
