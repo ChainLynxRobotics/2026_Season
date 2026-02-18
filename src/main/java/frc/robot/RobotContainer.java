c6918f42b5319c916e94267e790abc62d5b4b011
Carter Mackey <carter.mackey@icloud.com>
2026-02-18 15:26:19 -0800

    fix rebase & comment shooter test that doesnt work on real gains

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -12,30 +12,28 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 import com.pathplanner.lib.auto.NamedCommands;
 import com.pathplanner.lib.commands.PathPlannerAuto;
 import edu.wpi.first.epilogue.Logged;
-import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.units.measure.Voltage;
 import edu.wpi.first.wpilibj.RobotBase;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
 import frc.robot.generated.TunerConstants;
 import frc.robot.subsystems.Indexer.IndexerSubsystem;
-import frc.robot.subsystems.Intake.IntakeConstants.IntakeHeightState;
 import frc.robot.subsystems.Intake.IntakeSubsystem;
 import frc.robot.subsystems.Serializer.SerializerSubsystem;
 import frc.robot.subsystems.Shooter.Shooter;
 import frc.robot.subsystems.Shooter.ShooterConstants;
 import frc.robot.subsystems.Swerve.CommandSwerveDrivetrain;
+import frc.robot.subsystems.Swerve.DrivetrainConstants;
 import org.ironmaple.simulation.SimulatedArena;
 import org.ironmaple.simulation.motorsims.SimulatedBattery;
-import frc.robot.subsystems.Drivetrain.DrivetrainConstants;
-import frc.robot.subsystems.Swerve.CommandSwerveDrivetrain;
 
 @Logged
 public class RobotContainer {
@@ -172,20 +170,10 @@ public class RobotContainer {
 
     drivetrain.registerTelemetry(logger::telemeterize);
 
-    new Trigger(() -> intakeSimJoystick.getRawButton(1)).onTrue(intake.spin());
-
-    new Trigger(() -> intakeSimJoystick.getRawButton(2))
-        .onTrue(intake.setHeight(IntakeHeightState.LOW));
-
-    new Trigger(() -> intakeSimJoystick.getRawButton(3))
-        .onTrue(intake.setHeight(IntakeHeightState.HIGH));
-
-    new Trigger(() -> intakeSimJoystick.getRawButton(4)).onTrue(indexer.spin());
-
     operatorJoystick.a().whileTrue(shooter.hoodSysid());
     operatorJoystick.b().onTrue(shooter.setFlywheelVelocity(RotationsPerSecond.of(20)));
     operatorJoystick.x().onTrue(shooter.setHoodAngle(Degrees.of(60)));
-    operatorJoystick.y().onTrue(runOnce(shooter::shootSimulatedProjectile));
+    operatorJoystick.y().onTrue(Commands.runOnce(shooter::shootSimulatedProjectile));
 
     operatorController.a().onTrue(intake.spin());
     operatorController.b().onTrue(serializer.spin());
@@ -209,7 +197,6 @@ public class RobotContainer {
         .getTranslation()
         .getAngle();
   }
-
 
   public Command getAutonomousCommand() {
     return new PathPlannerAuto("0");
--- src/main/java/frc/robot/subsystems/Swerve/DrivetrainConstants.java
+++ src/main/java/frc/robot/subsystems/Swerve/DrivetrainConstants.java
@@ -1,4 +1,4 @@
-package frc.robot.subsystems.Drivetrain;
+package frc.robot.subsystems.Swerve;
 
 import static edu.wpi.first.units.Units.*;
 
