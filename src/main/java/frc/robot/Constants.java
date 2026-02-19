9f4a525ad2d179add5a54dedf346bc1093ac2087
Red laptop <devices@chainlynx8248.com>
2026-02-18 20:09:44 -0800

    Add CANBus's to constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -2,9 +2,12 @@ package frc.robot;
 
 import static edu.wpi.first.units.Units.*;
 
+import com.ctre.phoenix6.CANBus;
 import edu.wpi.first.units.measure.*;
 
 public class Constants {
   public static final Time kDT = Seconds.of(0.02);
   public static final boolean tuningMode = true;
+  public static final CANBus kCanBusBlinky = new CANBus("blinky");
+  public static final CANBus kCanBusRio = new CANBus("rio");
 }
