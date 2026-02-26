ec97bc3badf7dfefd817a42bf0108e24f9e383fe
Blue laptop <devices@chainlynx8248.com>
2026-02-25 18:57:57 -0800

    add more methods to RobotMath

--- src/main/java/frc/robot/utils/RobotMath.java
+++ src/main/java/frc/robot/utils/RobotMath.java
@@ -1,7 +1,11 @@
 package frc.robot.utils;
 
+import static edu.wpi.first.units.Units.*;
+
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Unit;
+import edu.wpi.first.units.measure.*;
 
 public class RobotMath {
   /**
@@ -94,5 +98,23 @@ public class RobotMath {
 
   public static <U extends Unit> boolean isMeasureNaN(Measure<U> measure) {
     return Double.isNaN(measure.baseUnitMagnitude());
+  }
+
+  public static boolean isPoseInSquare(Pose2d pose, Pose2d corner1, Pose2d corner2) {
+    var leftMostCorner = corner1.getX() <= corner2.getX() ? corner1 : corner2;
+    var rightMostCorner = corner1.getX() >= corner2.getX() ? corner1 : corner2;
+    var bottomMostCorner = corner1.getY() <= corner2.getY() ? corner1 : corner2;
+    var topMostCorner = corner1.getY() >= corner2.getY() ? corner1 : corner2;
+
+    return pose.getX() >= leftMostCorner.getX()
+        && pose.getX() <= rightMostCorner.getX()
+        && pose.getY() >= bottomMostCorner.getY()
+        && pose.getY() <= topMostCorner.getY();
+  }
+
+  public static final Distance getDistanceBetweenPoses(Pose2d pose1, Pose2d pose2) {
+    var relitivePose = pose1.relativeTo(pose2);
+    return Meters.of(
+        Math.sqrt(Math.pow(relitivePose.getX(), 2) + Math.pow(relitivePose.getY(), 2)));
   }
 }
