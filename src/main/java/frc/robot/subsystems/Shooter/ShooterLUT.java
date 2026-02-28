fa795baac316ab1f52f0be1382c113262caa293b
me-it-is <188520719+me-it-is@users.noreply.github.com>
2026-02-28 11:02:59 -0800

    update shooter tuning data

--- src/main/java/frc/robot/subsystems/Shooter/ShooterLUT.java
+++ src/main/java/frc/robot/subsystems/Shooter/ShooterLUT.java
@@ -66,14 +66,13 @@ public class ShooterLUT {
     if (RobotBase.isReal()) {
       // Joe: These values are supposed to be flywheel surface speed in m/s, but 60-80 seems really
       // high for m/s — are these actually RPS from the old shooter? Might need retuning.
-      map.put(1.8546, 60.0);
-      map.put(2.221275, 65.0);
-      map.put(2.647, 65.0);
-      map.put(2.934, 67.5);
-      map.put(3.333, 70.0);
-      map.put(3.70912, 75.0);
-      map.put(4.1389, 77.5);
-      map.put(4.5181, 80.0);
+      map.put(1.041, 60.0);
+      map.put(1.92, 65.0);
+      map.put(2.31, 65.0);
+      map.put(3.25, 72.0);
+      map.put(4.17, 78.0);
+      map.put(5.71, 92.0);
+      map.put(7.37, 103.0);
     } else {
       map.put(1.25, 25.2);
       map.put(1.5, 26.5);
@@ -94,14 +93,13 @@ public class ShooterLUT {
   private static InterpolatingDoubleTreeMap generateAngleMap() {
     var map = new InterpolatingDoubleTreeMap();
     if (RobotBase.isReal()) {
-      map.put(1.8546, 25.0);
-      map.put(2.221275, 25.0);
-      map.put(2.647, 27.5);
-      map.put(2.934, 27.5);
-      map.put(3.333, 27.5);
-      map.put(3.70912, 27.5);
-      map.put(4.1389, 30.0);
-      map.put(4.5181, 30.0);
+      map.put(1.041, 18.0);
+      map.put(1.92, 23.0);
+      map.put(2.31, 24.0);
+      map.put(3.25, 27.5);
+      map.put(4.17, 29.0);
+      map.put(5.71, 32.0);
+      map.put(7.37, 43.0);
     } else {
       map.put(1.5, 8.56);
       map.put(2.0, 11.32);
