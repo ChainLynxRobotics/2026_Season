030b562a188ccad8f281a091d613b19c46e7eb1d
Wrenn54 <benvubrown@gmail.com>
2026-02-27 19:09:48 -0800

    Util to detect current spikes (EXPERIMENTAL)

---
+++ src/main/java/frc/robot/utils/CurrentSpikeDetector.java
@@ -0,0 +1,47 @@
+package frc.robot.utils;
+
+import static edu.wpi.first.units.Units.Amps;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+import java.util.function.BooleanSupplier;
+
+public class CurrentSpikeDetector {
+  private double totalCurrent;
+  private double lastCurrent;
+  private int iterations = 0;
+
+  TalonFX talonFXMotor;
+
+  //   public CurrentSpikeDetector(TalonFX motor) {
+  //     this.talonFXMotor = motor;
+  //     }
+
+  public void recordCurrent(TalonFX motor) {
+    iterations++;
+    lastCurrent = motor.getSupplyCurrent().getValue().in(Amps);
+    totalCurrent += lastCurrent;
+  }
+
+  public double getAverageCurrent() {
+    return totalCurrent / iterations;
+  }
+
+  public BooleanSupplier detectSpike() {
+    if (iterations > 10) {
+      if (lastCurrent > getAverageCurrent() * 2) {
+        return () -> detectSpike(1);
+      }
+    }
+    return () -> false;
+  }
+
+  private boolean detectSpike(int timesCalled) {
+    if (timesCalled == 5) {
+      return true;
+    } else if (lastCurrent > getAverageCurrent() * 2) {
+      return detectSpike(timesCalled + 1);
+    } else {
+      return false;
+    }
+  }
+}
