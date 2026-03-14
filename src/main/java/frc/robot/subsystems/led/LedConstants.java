613b0314b58646d17ef966e83bfbf8132d8e3f3e
Blue laptop <devices@chainlynx8248.com>
2026-03-14 12:00:15 -0700

    fix led count

--- src/main/java/frc/robot/subsystems/led/LedConstants.java
+++ src/main/java/frc/robot/subsystems/led/LedConstants.java
@@ -2,7 +2,7 @@ package frc.robot.subsystems.led;
 
 public class LedConstants {
 
-  public static final int kLeds = 343;
+  public static final int kLeds = 339;
   public static final int kLEDPort = 0;
   public static final int kNumberOfPixels = 365; // should this be 343
 }
