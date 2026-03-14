9d80c252527290fd6cef84a64ba1a4820569606d
Blue laptop <devices@chainlynx8248.com>
2026-03-13 19:56:58 -0700

    Setup led

---
+++ src/main/java/frc/robot/subsystems/led/LedSubsystem.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems.led;
+
+import edu.wpi.first.wpilibj.AddressableLED;
+
+public class LedSubsystem {
+  public AddressableLED led = new AddressableLED(0);
+  //345 bulbs total
+}
