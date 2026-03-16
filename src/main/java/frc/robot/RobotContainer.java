f47f3f970c3f5bdcd5ecf78a5b6432f2a6092245
Carter Mackey <carter.mackey@icloud.com>
2026-03-15 20:41:10 -0700

    sotm auto velocity adjustment

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -475,7 +475,7 @@ public class RobotContainer {
                     .withTargetRateFeedforward(RotationsPerSecond.of(getProfile().velocity))
                     .withHeadingPID(
                         tunableHeadingP.get(), tunableHeadingI.get(), tunableHeadingD.get())
-                    .withVelocityX(-0.25)
+                    .withVelocityX(-0.5)
                     .withVelocityY(0)));
   }
 
