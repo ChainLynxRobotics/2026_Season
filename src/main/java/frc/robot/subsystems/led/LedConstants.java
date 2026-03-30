24f0df382892b827129a6783415c887dd7ef1eb4
DoraChad <176503791+DoraChad@users.noreply.github.com>
2026-03-29 19:26:06 -0700

    up led brownout voltage

--- src/main/java/frc/robot/subsystems/led/LedConstants.java
+++ src/main/java/frc/robot/subsystems/led/LedConstants.java
@@ -8,6 +8,6 @@ public class LedConstants {
 
   public static final int kLeds = 339;
   public static final int kLEDPort = 0;
-  public static final double kBrownOut = 9.5;
+  public static final double kBrownOut = 12;
   public static final Distance kLedSpacing = Meters.of(1 / 40.0);
 }
