6a6f1c77db7b84e9f0272e17d2fb8d1587614fb5
Blue laptop <devices@chainlynx8248.com>
2026-04-06 15:55:51 -0700

    update led count because uh rip

--- src/main/java/frc/robot/subsystems/led/LedConstants.java
+++ src/main/java/frc/robot/subsystems/led/LedConstants.java
@@ -6,7 +6,7 @@ import edu.wpi.first.units.measure.Distance;
 
 public class LedConstants {
 
-  public static final int kLeds = 339;
+  public static final int kLeds = 339-123;
   public static final int kLEDPort = 0;
   public static final double kBrownOut = 8;
   public static final Distance kLedSpacing = Meters.of(1 / 40.0);
